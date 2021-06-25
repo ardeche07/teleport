7f689c0ad893f140b576f9fec7c56c160695eb98
xacrimon <joel.wejdenstal@gmail.com>
2021-06-25 19:47:31 +0200

    use typed sessionid

--- lib/auth/auth_with_roles.go
+++ lib/auth/auth_with_roles.go
@@ -3150,7 +3150,7 @@ func (a *ServerWithRoles) DeleteAllLocks(context.Context) error {
 
 // StreamSessionEvents streams all events from a given session recording. An error is returned on the first
 // channel if one is encountered. Otherwise it is simply closed when the stream ends.
-func (a *ServerWithRoles) StreamSessionEvents(ctx context.Context, sessionID string, startIndex int) (chan apievents.AuditEvent, chan error) {
+func (a *ServerWithRoles) StreamSessionEvents(ctx context.Context, sessionID session.ID, startIndex int) (chan apievents.AuditEvent, chan error) {
 	if err := a.action(apidefaults.Namespace, types.KindSession, types.VerbList); err != nil {
 		e, c := make(chan error, 1), make(chan apievents.AuditEvent)
 		e <- trace.Wrap(err)
